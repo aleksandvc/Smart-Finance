7b33a52949513c6f5474c78ea6842e4c905503f1
Diana Ivanova <diana.sv.ivanova@gmail.com>
2017-04-05 14:52:09 +0300

    Formatted code

--- SmartFinance/app/src/main/java/com/vladimircvetanov/smartfinance/model/Manager.java
+++ SmartFinance/app/src/main/java/com/vladimircvetanov/smartfinance/model/Manager.java
@@ -4,26 +4,23 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.TreeMap;
 
-/**
- * Created by vladimircvetanov on 04.04.17.
- */
-
 public class Manager {
 
     private static Manager instance = null;
-    public interface IType{}
-    public enum Category implements IType{VEHICLE,CLOTHES,HEALTH,TRAVEL,SPORT,FOOD,TRANSPORT,PHONE,HOUSE,ENTERTAIMENT};
-    public enum Type{INCOMING,EXPENSE};
-    private HashMap<Type,HashMap<IType,TreeMap<Date,Double>>> logs;
+    public interface IType {}
 
-    private Manager(){
+    public enum Category implements IType {VEHICLE, CLOTHES, HEALTH, TRAVEL, SPORT, FOOD, TRANSPORT, PHONE, HOUSE, ENTERTAINMENT}
+    public enum Type {INCOMING, EXPENSE}
+    private HashMap<Type, HashMap<IType, TreeMap<Date, Double>>> logs;
+
+    private Manager() {
         logs = new HashMap<>();
-        logs.put(Type.INCOMING,new HashMap<IType, TreeMap<Date, Double>>());
-        logs.put(Type.EXPENSE,new HashMap<IType, TreeMap<Date, Double>>());
+        logs.put(Type.INCOMING, new HashMap<IType, TreeMap<Date, Double>>());
+        logs.put(Type.EXPENSE, new HashMap<IType, TreeMap<Date, Double>>());
     }
 
-    public static Manager getInstance(){
-        if(instance == null){
+    public static Manager getInstance() {
+        if (instance == null) {
             instance = new Manager();
         }
         return instance;
