c33b9724731e1d900d6bcda6552edcab3fb4b18b
Diana Ivanova <diana.sv.ivanova@gmail.com>
2017-04-06 12:59:19 +0300

    Toolbar added

--- SmartFinance/app/src/main/java/com/vladimircvetanov/smartfinance/MainActivity.java
+++ SmartFinance/app/src/main/java/com/vladimircvetanov/smartfinance/MainActivity.java
@@ -2,12 +2,42 @@ package com.vladimircvetanov.smartfinance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_currency:
+                return true;
+            case R.id.item_settings:
+                return true;
+            case R.id.item_logout:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
